f3c771e7e9cfddfb1ea533c39ecdb3cf9e6e5375
Leeor Baskin <lbaskin@gmail.com>
2015-07-10 00:37:17 -0400

    Resize bmp
    Increase size of bmp file by a factor (integer) provided by user

--- pset4/bmp/whodunit.c
+++ pset4/bmp/whodunit.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     // ensure proper usage
     if (argc != 3)
     {
-        printf("Usage: ./copy infile outfile\n");
+        printf("Usage: ./whodunit infile outfile\n");
         return 1;
     }
 
